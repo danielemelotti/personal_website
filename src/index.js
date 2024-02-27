bfb06c9f39cee5d8d6744a9a3a82efd5fbf0241f
melottidaniele <danielemelotti@hotmail.com>
2024-02-27 23:48:54 +0800

    Create repository and push all files

--- src/index.js
+++ src/index.js
@@ -4,3 +4,62 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+const heroTitle = document.querySelector('.hero-title');
+const staticText = heroTitle.innerHTML; // Get the existing HTML
+const phrases = [
+  "Welcome to my website!",
+  "I am a Data Scientist constantly looking for new projects and opportunities.",
+];
+const finalLine = "Together, let's transform our vision into an astounding reality!"
+
+let phraseIndex = 0;
+let charIndex = 0; // Start from the beginning of the first phrase
+let isTyping = true; // Start with typing effect
+
+function typewriteEffect() {
+  if (phraseIndex < phrases.length) {
+    if (isTyping) {
+      // Typing
+      if (charIndex < phrases[phraseIndex].length) {
+        heroTitle.innerHTML = staticText + '<span class="dynamic-text">' + phrases[phraseIndex].substring(0, charIndex + 1);
+        charIndex++;
+        setTimeout(typewriteEffect, 45); // Typing speed
+      } else {
+        // Once finished typing a phrase, start untyping after a short delay
+        isTyping = false;
+        setTimeout(typewriteEffect, 1500); // Delay before untyping
+      }
+    } else {
+      // Untyping
+      if (charIndex > 0) {
+        charIndex--;
+        heroTitle.innerHTML = staticText + '<span class="dynamic-text">' + phrases[phraseIndex].substring(0, charIndex);
+        setTimeout(typewriteEffect, 15); // Untyping speed (5 times faster)
+      } else {
+        // Once finished untyping, move to the next phrase or the final line
+        phraseIndex++;
+        if (phraseIndex < phrases.length) {
+          isTyping = true;
+          setTimeout(typewriteEffect, 500); // Delay before typing next phrase
+        } else {
+          // Start typing the final line
+          charIndex = 0; // Reset for finalLine
+          setTimeout(typeFinalLine, 500); // Delay before starting finalLine
+        }
+      }
+    }
+  }
+}
+
+function typeFinalLine() {
+  if (charIndex < finalLine.length) {
+    heroTitle.innerHTML = staticText + '<span class="final-line">' + finalLine.substring(0, charIndex + 1) + '</span>'; // Use the new class
+    charIndex++;
+    setTimeout(typeFinalLine, 100); 
+  }
+  // No need to call setTimeout here again, it stops after finalLine is complete
+}
+
+// Initial call
+typewriteEffect();
